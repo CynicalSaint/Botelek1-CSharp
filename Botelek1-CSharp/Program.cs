7ac75851c47d87acf5f63247579938e0e5af5209
Rikki Lanouette <cynicalsaint.lol@gmail.com>
2018-04-26 18:12:21 +0100

    Initial Commit - Babby Botelek1

---
+++ Botelek1-CSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Botelek1_CSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
