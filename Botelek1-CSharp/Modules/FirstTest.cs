24a2158bde43967b0db077ca8dd588f1e79efd10
Rikki Lanouette <rlanouette@temenos.com>
2018-04-26 23:07:54 +0100

    Basic command testing

--- Botelek1-CSharp/Modules/FirstTest.cs
+++ Botelek1-CSharp/Modules/FirstTest.cs
@@ -1,7 +1,5 @@
-﻿using Discord.Commands;
-using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Discord;
+using Discord.Commands;
 using System.Threading.Tasks;
 
 namespace Botelek1_CSharp.Modules
@@ -13,5 +11,46 @@ namespace Botelek1_CSharp.Modules
         {
             await Context.Channel.SendMessageAsync($"Pong!");
         }
+
+        [Command("embed", RunMode = RunMode.Async)]
+        public async Task EmbedDemo([Remainder]string message)
+        {
+            EmbedBuilder embed = new EmbedBuilder();
+            string[] messageParts = message.Split(":");
+
+            if (messageParts.Length < 2 || messageParts.Length > 3)
+            {
+                embed.WithTitle("Error!");
+                embed.WithDescription("Incorrect syntax, command usage : !embed {title}:{message}:{colour}");
+                embed.WithColor(Color.Red);
+
+                await Context.Channel.SendMessageAsync($"New embedded object sent in {Context.Channel.Name} channel", false, embed.Build());
+            }
+
+            if (messageParts.Length == 2)
+            {
+                embed.WithTitle(messageParts[0]);
+                embed.WithDescription(messageParts[1]);
+                embed.WithColor(Color.Green);
+
+                await Context.Channel.SendMessageAsync($"New embedded object sent in {Context.Channel.Name} channel", false, embed.Build());
+            }
+
+            if (messageParts.Length == 3)
+            {
+                embed.WithTitle(messageParts[0]);
+                embed.WithDescription(messageParts[1]);
+                embed.WithColor(GetColour(messageParts[2]));
+
+                await Context.Channel.SendMessageAsync($"!embed Test:Test:cyan", false, embed.Build());
+            }
+        }
+
+        private Color GetColour(string colour)
+        {
+            System.Drawing.Color systemColour = System.Drawing.Color.FromName(colour.Trim());
+            return new Color(systemColour.R, systemColour.G, systemColour.B);
+        }
+
     }
 }
