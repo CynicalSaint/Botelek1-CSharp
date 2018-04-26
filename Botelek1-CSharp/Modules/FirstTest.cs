5c28d8f6811fcc170c1965e3c662747c7371e78b
Rikki Lanouette <rlanouette@temenos.com>
2018-04-26 22:02:36 +0100

    Beginning of test command module

---
+++ Botelek1-CSharp/Modules/FirstTest.cs
@@ -0,0 +1,11 @@
+﻿using Discord.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Botelek1_CSharp.Modules
+{
+    class FirstTest : ModuleBase
+    {
+    }
+}
